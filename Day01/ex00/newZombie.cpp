29ade3ad18e3da5df86a182e6dc4fb4c919504e1
Mohammed Benkhattab <77858811+Mohamaedbenk@users.noreply.github.com>
2022-08-26 23:42:11 +0100

    Delete Day01 directory

--- Day01/ex00/newZombie.cpp
+++
@@ -1,6 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* newZombie( std::string name ){
-    Zombie  *newZ = new Zombie(name);
-    return newZ;
-}
--- Day01/ex00/randomChump.cpp
+++
@@ -1,6 +0,0 @@
-#include "Zombie.hpp"
-
-void randomChump( std::string name ){
-    Zombie chump(name);
-    chump.announce();
-}
